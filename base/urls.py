6598df720a5f0188d93fe101f7b3d6503b30eaa3
Jeremias Schreiner <jeremias-ush@hotmail.com>
2022-06-24 08:38:12 -0300

    implementacion basica de todo_list, todo_detail y todo_create

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-
+    path('', views.TaskListView.as_view(), name="task-list"),
+    path('task/<int:pk>/', views.TaskDetailView.as_view(), name='task-detail'),
+    path('task-create', views.TaskCreateView.as_view(), name='task-create')
 ]
--- base/views.py
+++ base/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from base.models import Task
 
 # Create your views here.
+
+class TaskListView(ListView):
+    model = Task
+    context_object_name = 'tasks'
+    template_name = 'base/task-list.html'
+
+class TaskDetailView(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'base/task-detail.html'
+
+class TaskCreateView(CreateView):
+    model = Task
+    success_url = reverse_lazy('task-list')
+    template_name = 'base/task-form.html'
+    fields = '__all__'
